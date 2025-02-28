dec93319ed894b25e96f30ee477be291706b15d1
Neto Marina <koshkalisa2018@yandex.ru>
2025-02-28 19:03:04 +0300

    add README

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate(double weight, double height) {
+    public int calculate(int weight, double height) {
         double bmi = weight / (height * height);
         return (int) bmi;
     }
